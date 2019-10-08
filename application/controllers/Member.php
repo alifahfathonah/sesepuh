19e5d3f184356c2e8840efeff261b97a62f3ee43
sahyudi_dev <poetrageranting@gmail.com>
2019-10-08 23:44:58 +0700

    add update data member

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -13,6 +13,7 @@ class Member extends CI_Controller
         $data['title'] = 'List';
         $data['user'] = $this->db->get_where('tbl_user', ['email' => $this->session->userdata('email')])->row_array();
         $data['menu'] = $this->db->get('tbl_user_menu')->result_array();
+
         $this->form_validation->set_rules('menu', 'Menu', 'required');
 
         if ($this->form_validation->run() == false) {
@@ -20,6 +21,8 @@ class Member extends CI_Controller
             $this->db->from('tbl_user AS A');
             $this->db->join('tbl_user_role AS B', 'A.role_id = B.id');
             $data['member'] = $this->db->get()->result_array();
+            $data['role'] = $this->db->get('tbl_user_role')->result_array();
+
             $this->load->view('template/header', $data);
             $this->load->view('template/sidebar', $data);
             $this->load->view('template/topbar', $data);
@@ -32,45 +35,25 @@ class Member extends CI_Controller
         }
     }
 
-    public function submenu()
+    public function update()
     {
-        $data['title'] = 'Submenu Managament';
-        $data['user'] = $this->db->get_where('tbl_user', ['email' => $this->session->userdata('email')])->row_array();
+        $data = [
+            'name' => $this->input->post('name'),
+            'role_id' => $this->input->post('role')
+        ];
 
-        $this->load->model('Menu_model', 'menu');
-
-        $data['submenu'] = $this->menu->getSubMenu();
-        $data['menu'] = $this->db->get('tbl_user_menu')->result_array();
-
-        $this->form_validation->set_rules('title', 'Title', 'required');
-        $this->form_validation->set_rules('menu_id', 'Menu', 'required');
-        $this->form_validation->set_rules('url', 'URL', 'required');
-        $this->form_validation->set_rules('icon', 'Icon', 'required');
-
-        if ($this->form_validation->run() == false) {
-            $this->load->view('template/header', $data);
-            $this->load->view('template/sidebar', $data);
-            $this->load->view('template/topbar', $data);
-            $this->load->view('menu/submenu', $data);
-            $this->load->view('template/footer');
+        if ($this->db->update('tbl_user', $data, ['email' => $this->input->post('email')])) {
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data Member updated!</div>');
         } else {
-            $data = [
-                'title' => $this->input->post('title'),
-                'menu_id' => $this->input->post('menu_id'),
-                'url' => $this->input->post('url'),
-                'icon' => $this->input->post('icon'),
-                'is_active' => $this->input->post('is_active')
-            ];
-
-            $this->db->insert('tbl_user_sub_menu', $data);
-            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Menu new added!</div>');
-            redirect('menu/submenu');
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Data Member failed update!</div>');
         }
+
+        redirect('member');
     }
 
-    public function date_time()
+    public function editMember($id)
     {
-        var_dump(time());
-        die;
+        $data = $this->db->get_where('tbl_user', ['id' => $id])->row_array();
+        echo json_encode($data);
     }
 }
--- application/views/member/list-member.php
+++ application/views/member/list-member.php
@@ -10,7 +10,7 @@
 
             <?= $this->session->flashdata('message'); ?>
 
-            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newMenuModal">Add New Baru</a>
+            <!-- <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newMenuModal">Add New Baru</a> -->
             <table class="table table-hover ">
                 <thead>
                     <tr>
@@ -39,7 +39,7 @@
                             <td><?= $status ?></td>
                             <td><?= date('d F Y', $m['date_created']); ?></td>
                             <td>
-                                <a href="#" class="badge badge-success">Edit</a>
+                                <a href="#" data-id="<?= $m['id']; ?>" data-target="#edit-member" data-toggle="modal" class="badge badge-success btn-edit">Edit</a>
                                 <a href="<?= base_url('member/deleteMember/') . $m['id'] ?>" onclick="return myFunction()" class="badge badge-danger">delete</a>
                             </td>
                         </tr>
@@ -55,32 +55,60 @@
 </div>
 <!-- End of Main Content -->
 <!-- Modal -->
-<div class="modal fade" id="newMenuModal" tabindex="-1" role="dialog" aria-labelledby="newMenuModal" aria-hidden="true">
+<div class="modal fade" id="edit-member" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="neMenuModalLabel">Add New Menu</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="close">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Member</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form action="<?= base_url('menu') ?>" method="post">
+            <form action="<?= base_url('member/update') ?>" method="post">
                 <div class="modal-body">
                     <div class="form-group">
-                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Menu name">
+                        <label for="recipient-name" class="col-form-label">Email</label>
+                        <input type="text" class="form-control" id="email" name="email" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="recipient-name" class="col-form-label">Nama</label>
+                        <input type="text" class="form-control" name="name" id="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="message-text" class="col-form-label">Role</label>
+                        <select name="role" id="role" class="form-control" data-placeholder="Select One" id="role">
+                            <option value=""></option>
+                            <?php foreach ($role as $key) { ?>
+                                <option value="<?= $key['id'] ?>"><?= $key['role'] ?></option>
+                            <?php } ?>
+                        </select>
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">close</button>
-                    <button class="btn btn-primary" type="submit">Add New Menu</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save Changes</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
-
+<script src="<?= base_url('assets/') ?>vendor/jquery/jquery.min.js"></script>
 <script>
     function myFunction() {
         return confirm('Apakah anda yakin akan menghapus ?');
     }
+
+    $('.btn-edit').on('click', function() {
+        const id = $(this).data('id');
+        $.ajax({
+            url: "<?= base_url('member/editMember/') ?>" + id,
+            type: "post",
+            dataType: 'JSON',
+            success: function(data) {
+                $('#email').val(data.email);
+                $('#name').val(data.name);
+                $('#role').val(data.role_id);
+            }
+        })
+    });
 </script>
