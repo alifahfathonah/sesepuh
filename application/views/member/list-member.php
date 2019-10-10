2e06c2243d67002a165d323962680c04c1d42873
sahyudi_dev <poetrageranting@gmail.com>
2019-10-11 06:25:53 +0700

    Menggantikan id modal untuk edit data member

--- application/views/member/list-member.php
+++ application/views/member/list-member.php
@@ -55,7 +55,7 @@
 </div>
 <!-- End of Main Content -->
 <!-- Modal -->
-<div class="modal fade" id="create" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="edit-member" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
